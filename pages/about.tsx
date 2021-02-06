354b6f04ffc0046d9278fd9263b422c14e8333d1
kazuyaseki <track613armory@gmail.com>
2021-02-06 22:46:12 +0900

    initial commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
+    <h1 className="flex-auto text-xl text-gray-500">About</h1>
     <p>This is the about page</p>
     <p>
       <Link href="/">
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      "grey-100": '#F7FAFC',
+      "grey-200": '#EDF2F7',
+      "grey-300": '#E2E8F0',
+      "grey-400": '#CBD5E0',
+      "grey-500": '#A0AEC0',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
